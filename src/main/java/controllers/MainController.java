117cfcb25258a61003dda2d246012c1fbf0d1c2b
Alexis220382 <Alexis220382@gmail.com>
2015-08-20 19:20:12 +0200

    first commit

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -5,7 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.mail.*;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.Properties;
 
 @Controller
 public class MainController {
@@ -23,14 +27,47 @@ public class MainController {
 
         if (request.getParameter("lastName").length() != 0
                 && request.getParameter("firstName").length() != 0
-<<<<<<< HEAD
                 && request.getParameter("secondName").length() != 0
                 && request.getParameter("lastName").toUpperCase().equals(request.getParameter("lastName"))
                 && request.getParameter("firstName").toUpperCase().equals(request.getParameter("firstName"))
                 && request.getParameter("secondName").toUpperCase().equals(request.getParameter("secondName"))){
-=======
-                && request.getParameter("secondName").length() != 0){
->>>>>>> origin/master
+
+
+            String SMPT_HOSTNAME = "smtp.gmail.com";
+            final String USERNAME = "220382alexis@gmail.com";
+            final String PASSWORD = "220382lesha";
+
+            Properties props = new Properties();
+            props.put("mail.smtp.host", SMPT_HOSTNAME);
+            props.put("mail.from","220382alexis@gmail.com");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.debug", "true");
+
+            Session session = Session.getInstance(props, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(USERNAME, PASSWORD);
+                }
+            });
+            try {
+                MimeMessage msg = new MimeMessage(session);
+                msg.setFrom();
+                msg.setRecipients(Message.RecipientType.TO,
+                        "pavel.veinik@gmail.com");
+                msg.setSubject("Result RegForm");
+                msg.setSentDate(new Date());
+                msg.setText("New man is registered\nLast name: " + request.getParameter("lastName")
+                                + "\nFirst name: " + request.getParameter("firstName")
+                                + "\nSecond name: " + request.getParameter("secondName")
+                                + "\nAge: " + request.getParameter("age")
+                                + "\nProfession: " + request.getParameter("profession")
+                                + "\nPhone: +375" + request.getParameter("phone"));
+                Transport.send(msg);
+            } catch (MessagingException mex) {
+                System.out.println("send failed, exception: " + mex);
+            }
+
             modelAndView.setViewName("answergood");
         }else{
             modelAndView.setViewName("answerbad");
